cf9060983f6804e2fcd7a7544218ec0af361a749
chomosuke <a13323600@gmail.com>
2023-02-05 11:09:35 +1100

    testing continuous deployment

--- app/page.tsx
+++ app/page.tsx
@@ -72,19 +72,9 @@ export default function Home() {
           <p className={inter.className}>Explore the Next.js 13 playground.</p>
         </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <h2 className={inter.className}>
+          This is deployed on AWS ECS.
+        </h2>
       </div>
     </main>
   )
