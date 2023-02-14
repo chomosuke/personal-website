f8585cc6088e210b143d1f8a21c3a36958790802
chomosuke <a13323600@gmail.com>
2023-02-14 19:23:24 +1100

    GridMorph basic animation achieved

--- lib/grid_morph.dart
+++ lib/grid_morph.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:fraction/fraction.dart';
 import 'package:styled_widget/styled_widget.dart';
 
 class GridMorph extends HookWidget {
@@ -19,7 +20,13 @@ class GridMorph extends HookWidget {
 
     final iFocused = useState<int?>(null);
     final jFocused = useState<int?>(null);
+    final controller = useAnimationController(
+      duration: const Duration(seconds: 1),
+    );
+    final animation = CurvedAnimation(parent: controller, curve: Curves.ease);
+    final flex = useAnimation(animation);
 
+    final flexFraction = Fraction.fromDouble(flex + 1).reduce();
     return Column(
       children: [
         for (var i = 0; i < numRow; i++)
@@ -30,13 +37,14 @@ class GridMorph extends HookWidget {
                   onTap: () {
                     iFocused.value = i;
                     jFocused.value = j;
+                    controller.forward(from: 0);
                   },
                 ).expanded(
-                  flex: j == jFocused.value ? 2 : 1,
+                  flex: j == jFocused.value ? flexFraction[0] : flexFraction[1],
                 )
             ],
           ).expanded(
-            flex: i == iFocused.value ? 2 : 1,
+            flex: i == iFocused.value ? flexFraction[0] : flexFraction[1],
           )
       ],
     );
