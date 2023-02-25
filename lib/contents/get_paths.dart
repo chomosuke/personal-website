ebc7f1a84403e7c6d0901ac2360cffdb8fa03075
chomosuke <a13323600@gmail.com>
2023-02-26 03:55:54 +1100

    customized skills & works order

--- lib/contents/get_paths.dart
+++ lib/contents/get_paths.dart
@@ -3,15 +3,8 @@ import 'dart:convert';
 import 'package:flutter/services.dart';
 
 Future<List<String>> getPaths(String folder) async {
-  final list = (jsonDecode(
-    await rootBundle.loadString('AssetManifest.json'),
-  ) as Map<String, dynamic>)
-      .keys;
-  return list
-      .where((String k) => k.contains('content/$folder/'))
-      .where((String k) => k.contains('.md'))
-      .map(
-        (String skillPath) => skillPath.substring(8, skillPath.length - 3),
-      )
+  return const LineSplitter()
+      .convert(await rootBundle.loadString('content/$folder/order.txt'))
+      .map((line) => '$folder/$line')
       .toList();
 }
